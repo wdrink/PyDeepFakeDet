614d1d50e82a64178b55a109fdea53f9d7a9afae
zhangchaosd <zhangchaogoo@gmail.com>
2022-10-30 15:35:05 +0800

    Update augs

--- PyDeepFakeDet/datasets/utils.py
+++ PyDeepFakeDet/datasets/utils.py
@@ -3,21 +3,30 @@ import albumentations.pytorch
 import numpy as np
 from PIL import Image
 
-def get_augmentations_from_list(augs: list, aug_cfg, one_of_p=1):
+def get_augmentations_list_from_dict(augs: dict):
     ops = []
-    for aug in augs:
-        if isinstance(aug, list):
-            op = albumentations.OneOf
-            param = get_augmentations_from_list(aug, aug_cfg)
-            param = [param, one_of_p]
-        else:
+    keys = augs.keys()
+    for aug in keys:
+        try:
             op = getattr(albumentations, aug)
-            param = (
-                aug_cfg[aug.upper() + '_PARAMS']
-                if aug.upper() + '_PARAMS' in aug_cfg
-                else []
-            )
-        ops.append(op(*tuple(param)))
+        except:
+            print(f'No aug named {aug}')  # TODO
+            continue
+        if issubclass(op, albumentations.BaseCompose):
+            sub_augs = augs[aug]
+            p = None
+            if 'P' in sub_augs:
+                p = sub_augs['P']
+                sub_augs.pop('P')
+            sub_augs_list = get_augmentations_list_from_dict(sub_augs)
+            if p is None:
+                op = op(sub_augs_list)
+            else:
+                op = op(sub_augs_list, p)
+        else:
+            params = tuple(augs[aug]) if augs[aug] is not None else []
+            op = op(*params)
+        ops.append(op)
     return ops
 
 
@@ -29,7 +38,7 @@ def get_transformations(
         aug_cfg = dataset_cfg['TRAIN_AUGMENTATIONS']
     else:
         aug_cfg = dataset_cfg['TEST_AUGMENTATIONS']
-    ops = get_augmentations_from_list(aug_cfg['COMPOSE'], aug_cfg)
+    ops = get_augmentations_list_from_dict(aug_cfg)
     ops.append(albumentations.pytorch.transforms.ToTensorV2())
     augmentations = albumentations.Compose(ops, p=1)
     return augmentations
